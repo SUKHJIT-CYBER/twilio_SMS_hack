71dd13a67a578424bf6795402dcf1cf91d20d0ba
SUKHJIT SINGH <78156807+SUKHJIT-CYBER@users.noreply.github.com>
2021-10-24 09:00:24 +0530

    Updated Credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-account_sid = 'AC36e63bcfd1ccd075e2d99cc68b7ef438'
-auth_token= 'ba7e9496d4d6a4b05c155b19ed8d619f'
-my_cell='+91 76960 76904'
-my_twilio='+12182922942'
+account_sid = 'Your_Sid'
+auth_token= 'Your_token'
+my_cell='Your_cell'
+my_twilio='your_twilio'
